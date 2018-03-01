5b147c6d8dbde3438e8e05c0aed82f50368b035f
Liang Jian <15934854815@163.com>
2018-03-01 19:40:30 +0800

    stock

--- application/route.php
+++ application/route.php
@@ -38,6 +38,7 @@ Route::group("index", function () {
 Route::group("user", function () {
     Route::any('home','index/User/index'); //用户中心
     Route::any('optional','index/User/optional'); //自选
+    Route::any('add-optional','index/User/createOptional'); //添加自选
     Route::any('setting','index/User/setting'); //设置
     Route::any('password','index/User/password'); //修改密码
     Route::any('recharge','index/User/recharge'); //充值
