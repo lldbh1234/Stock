e26caadb2326c0dbf4f5c4239fce6b7ce50027a1
liangjian <15934854815@163.com>
2018-03-03 17:40:03 +0800

    ai

--- application/index/controller/Ai.php
+++ application/index/controller/Ai.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 
+use app\index\logic\StockLogic;
 use think\Request;
 use app\index\logic\AiLogic;
 
@@ -15,7 +16,20 @@ class Ai extends Base
 
     public function index()
     {
+        $codes = [];
         $lists = $this->_logic->aiTypeLists();
+        array_filter($lists, function(&$item) use (&$codes){
+            foreach ($item['has_many_ai'] as $value){
+                $codes[] = $value['code'];
+            }
+        });
+        $simple = (new StockLogic())->simpleData($codes);
+        array_filter($lists, function(&$item) use ($simple){
+            foreach ($item['has_many_ai'] as &$value){
+                $value['last_px'] = $simple[$value['code']]['last_px'];
+                $value['rate'] = round((($value['last_px'] - $value['income']) / $value['income'] * 100), 2);
+            }
+        });
         $this->assign("datas", $lists);
         return view();
     }
