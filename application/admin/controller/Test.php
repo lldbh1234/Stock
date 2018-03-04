36b4f7de1f84a5ab697bafd1e0bb4b49d5276f89
bruce <376394074@qq.com>
2018-03-05 01:32:17 +0800

    debug

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\admin\controller;
 
+use think\Queue;
 use think\Request;
 
 class Test extends Base
@@ -12,7 +13,15 @@ class Test extends Base
 
     public function test()
     {
-        self::checkAuth();
+        //两个方法，前者是立即执行，后者是在$delay秒后执行
+        //php think queue:listen
+        //php think queue:work --daemon（不加--daemon为执行单个任务）
+        //php think queue:work --queue helloJobQueue
+        $data = json_encode(['name' => 'test']);
+        $queue = null;
+
+        Queue::push('app\job\demoJob@fire', $data, $queue);
+        echo 'ok';
     }
 
 }
