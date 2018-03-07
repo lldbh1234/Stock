6f5c82a39f8a63d22ed4a6c4ad0ef43538a0bb9e
Liang Jian <15934854815@163.com>
2018-03-07 17:56:02 +0800

    plate

--- shell/plate.sh
+++ shell/plate.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #* * * * * www /home_path/shell/plate.sh >> /dev/null 2>&1
 
-step=3 #间隔的秒数，不能大于60    
-    
+step=5 #间隔的秒数，不能大于60
 for (( i = 0; i < 60; i=(i+step) )); do
     $(curl 'http://localhost/cron/plate')
     sleep $step
 done
-
 exit 0
