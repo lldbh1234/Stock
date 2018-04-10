08b426c908d600243283a51a5060c0bdeb13f59b
Liang Jian <15934854815@163.com>
2018-04-10 11:29:18 +0800

    job

--- application/index/job/UserNotice.php
+++ application/index/job/UserNotice.php
@@ -63,7 +63,7 @@ class UserNotice
                         "content" => "您关注的牛人“{$niuren['username']}”有新的操盘动向，请注意查看！",
                     ];
                 }
-//                model("UserNotice")->saveAll($saveData);
+                model("UserNotice")->saveAll($saveData);
             }
         }else{
             $readyKey = $data['order_id'].'_'.$data['type'];
@@ -76,7 +76,8 @@ class UserNotice
                 ];
             }
         }
-        model("UserNotice")->saveAll($saveData);
+        (new \app\index\model\UserNotice())->saveAll($saveData);
+        //model("UserNotice")->saveAll($saveData);
         return true;
     }
     public function checkNotice($option)
