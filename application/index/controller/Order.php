a18181e8c84e3bae396f7f8f9dfa8cd9467a5c50
Liang Jian <15934854815@163.com>
2018-04-25 17:40:19 +0800

    buy sell

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -351,7 +351,9 @@ class Order extends Base
                     while (true){
                         $quotation = (new StockLogic())->quotationBySina($order['code']);
                         if(isset($quotation[$order['code']]) && !empty($quotation[$order['code']])){
-                            $order['buy_px'] = $quotation[$order['code']]['buy_px']; // 平仓按买1价处理
+                            $last_px = $quotation[$order['code']]['last_px']; // 最新价
+                            $buy_px = $quotation[$order['code']]['buy_px']; // 平仓按买1价处理
+                            $order['buy_px'] = $last_px - $buy_px > 0.02 ? $buy_px + 0.02 : $buy_px; //买1如果比股票报价低，超过0.02 就上浮，反之不上调，等值也不上调
                             $res = $this->_userLogic->userOrderSelling($order);
                             if($res){
                                 return $this->ok();
--- application/index/controller/Stock.php
+++ application/index/controller/Stock.php
@@ -31,78 +31,91 @@ class Stock extends Base
                 $leverId = input("post.lever/d");
                 $price = input("post.price/f");
                 $followId = input("post.follow_id/d", 0);
-                $stock = $this->_logic->stockByCode($code);
-                $mode = (new ModeLogic())->modeIncPluginsById($modeId);
-                $deposit = (new DepositLogic())->depositById($depositId);
-                $lever = (new LeverLogic())->leverById($leverId);
-                $plugins = $mode['has_one_plugins'];
-                require_once request()->root() . "../plugins/{$plugins['type']}/{$plugins['code']}.php";
-                $obj = new $plugins['code'];
-                $trade = $obj->getTradeInfo($price, cf("capital_usage", 95), $deposit['money'], $lever['multiple'], $mode['jiancang'], $mode['defer']);
-                if(uInfo()['account'] >= $deposit['money'] + $trade["jiancang"]){
-                    $holiday = explode(',', cf("holiday", ""));
-                    $timestamp = workTimestamp($mode['free'], $holiday, strtotime(date("Y-m-d 14:40", request()->time())));
-                    $order = [
-                        "order_sn" => createStrategySn(),
-                        "user_id" => $this->user_id,
-                        "product_id" => $mode['product_id'],
-                        "mode_id" => $modeId,
-                        "lever" => $lever['multiple'],
-                        "code"  => $code,
-                        "name"  => $stock['name'],
-                        "full_code" => $stock['full_code'],
-                        "price" => $price,
-                        "hand"  => $trade["hand"],
-                        "jiancang_fee" => $trade["jiancang"],
-                        "defer" => $trade["defer"],
-                        "free_time" => $timestamp,
-                        "original_free" => $timestamp,
-                        "is_defer" => input("post.defer/d"),
-                        "stop_profit_price" => input("post.profit/f"),
-                        "stop_profit_point" => round((input("post.profit/f") - $price) / $price * 100, 2),
-                        "stop_loss_price" => input("post.loss/f"),
-                        "stop_loss_point" => round(($price - input("post.loss/f")) / $price * 100, 2),
-                        "deposit"   => $deposit['money'],
-                        "original_deposit" => $deposit['money'],
-                        "state"     => 3, // 下单即持仓
-                        "is_follow" => $followId ? 1 : 0,
-                        "follow_id" => $followId,
-                        "is_hedging" => 0, // 持仓单默认未对冲
-                    ];
-                    $orderId = (new OrderLogic())->createOrder($order);
-                    if($orderId > 0){
-                        $url = url("index/Order/position");
-                        // 队列
-                        $smsNoticeData = $sysNoticeData = ["niurenId" => $this->user_id];
-                        Queue::push('app\index\job\UserNotice@systemNotice', $sysNoticeData, null);
-                        Queue::push('app\index\job\UserNotice@smsNotice', $smsNoticeData, null);
-                        return $this->ok(["url" => $url]);
+                while(true){
+                    $quotation = $this->_logic->quotationBySina($code);
+                    if(isset($quotation[$code])){
+                        $last_px = $quotation[$code]['last_px'];
+                        $price = $last_px - $price > 0.02 ? $price + 0.02 : $price; //卖1如果比股票报价低，超过0.02 就上浮，反之不上调，等值也不上调
+                        $stock = $this->_logic->stockByCode($code);
+                        $mode = (new ModeLogic())->modeIncPluginsById($modeId);
+                        $deposit = (new DepositLogic())->depositById($depositId);
+                        $lever = (new LeverLogic())->leverById($leverId);
+                        $plugins = $mode['has_one_plugins'];
+                        require_once request()->root() . "../plugins/{$plugins['type']}/{$plugins['code']}.php";
+                        $obj = new $plugins['code'];
+                        $trade = $obj->getTradeInfo($price, cf("capital_usage", 95), $deposit['money'], $lever['multiple'], $mode['jiancang'], $mode['defer']);
+                        if(uInfo()['account'] >= $deposit['money'] + $trade["jiancang"]){
+                            $holiday = explode(',', cf("holiday", ""));
+                            $timestamp = workTimestamp($mode['free'], $holiday, strtotime(date("Y-m-d 14:40", request()->time())));
+                            $order = [
+                                "order_sn" => createStrategySn(),
+                                "user_id" => $this->user_id,
+                                "product_id" => $mode['product_id'],
+                                "mode_id" => $modeId,
+                                "lever" => $lever['multiple'],
+                                "code"  => $code,
+                                "name"  => $stock['name'],
+                                "full_code" => $stock['full_code'],
+                                "price" => $price,
+                                "hand"  => $trade["hand"],
+                                "jiancang_fee" => $trade["jiancang"],
+                                "defer" => $trade["defer"],
+                                "free_time" => $timestamp,
+                                "original_free" => $timestamp,
+                                "is_defer" => input("post.defer/d"),
+                                "stop_profit_price" => input("post.profit/f"),
+                                "stop_profit_point" => round((input("post.profit/f") - $price) / $price * 100, 2),
+                                "stop_loss_price" => input("post.loss/f"),
+                                "stop_loss_point" => round(($price - input("post.loss/f")) / $price * 100, 2),
+                                "deposit"   => $deposit['money'],
+                                "original_deposit" => $deposit['money'],
+                                "state"     => 3, // 下单即持仓
+                                "is_follow" => $followId ? 1 : 0,
+                                "follow_id" => $followId,
+                                "is_hedging" => 0, // 持仓单默认未对冲
+                            ];
+                            $orderId = (new OrderLogic())->createOrder($order);
+                            if($orderId > 0){
+                                $url = url("index/Order/position");
+                                // 队列
+                                $smsNoticeData = $sysNoticeData = ["niurenId" => $this->user_id];
+                                Queue::push('app\index\job\UserNotice@systemNotice', $sysNoticeData, null);
+                                Queue::push('app\index\job\UserNotice@smsNotice', $smsNoticeData, null);
+                                return $this->ok(["url" => $url]);
+                            }else{
+                                return $this->fail("创建策略失败！");
+                            }
+                        }else{
+                            return $this->fail("您的余额不足，请充值！");
+                        }
+                        break;
                     }else{
-                        return $this->fail("创建策略失败！");
+                        continue;
                     }
-                }else{
-                    return $this->fail("您的余额不足，请充值！");
                 }
             }
         }else{
             $stock = $this->_logic->stockByCode($code);
             if($stock){
-                //$quotation = $this->_logic->simpleData($code);
-                $quotation = $this->_logic->quotationBySina($code);
-                if(isset($quotation[$code]) && !empty($quotation[$code])){
-                    $modes = (new ModeLogic())->productModes();
-                    $deposits = (new DepositLogic())->allDeposits();
-                    $levers = (new LeverLogic())->allLevers();
-                    $this->assign("stock", $quotation[$code]);
-                    $this->assign("modes", $modes);
-                    $this->assign("deposits", $deposits);
-                    $this->assign("levers", $levers);
-                    $this->assign("user", uInfo());
-                    $this->assign("usage", cf('capital_usage', 95));
-                    $this->assign("follow_id", input("?follow_id") ? input("follow_id") : 0);
-                    return view('buy');
-                }else{
-                    return view('public/error');
+                while (true){
+                    //$quotation = $this->_logic->simpleData($code);
+                    $quotation = $this->_logic->quotationBySina($code);
+                    if(isset($quotation[$code]) && !empty($quotation[$code])){
+                        $modes = (new ModeLogic())->productModes();
+                        $deposits = (new DepositLogic())->allDeposits();
+                        $levers = (new LeverLogic())->allLevers();
+                        $this->assign("stock", $quotation[$code]);
+                        $this->assign("modes", $modes);
+                        $this->assign("deposits", $deposits);
+                        $this->assign("levers", $levers);
+                        $this->assign("user", uInfo());
+                        $this->assign("usage", cf('capital_usage', 95));
+                        $this->assign("follow_id", input("?follow_id") ? input("follow_id") : 0);
+                        return view('buy');
+                        break;
+                    }else{
+                        continue;
+                    }
                 }
             }else{
                 return view('public/error');
@@ -114,12 +127,15 @@ class Stock extends Base
     {
         $stock = $this->_logic->stockByCode($code);
         if($stock){
-            $quotation = $this->_logic->realTimeData($code);
-            if(isset($quotation[0]) && !empty($quotation[0])){
-                $this->assign("quotation", $quotation[0]);
-                return view();
-            }else{
-                return view('public/error');
+            while (true){
+                $quotation = $this->_logic->realTimeData($code);
+                if(isset($quotation[0]) && !empty($quotation[0])){
+                    $this->assign("quotation", $quotation[0]);
+                    return view();
+                    break;
+                }else{
+                    continue;
+                }
             }
         }else{
             return view('public/error');
