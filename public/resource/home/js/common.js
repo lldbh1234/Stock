7a13cd67efa5c2086eb88f29302fa81c5fcb9935
zhangguibao <13484938079@163.com>
2018-06-21 15:40:52 +0800

    xxx

--- public/resource/home/js/common.js
+++ public/resource/home/js/common.js
@@ -87,4 +87,55 @@ function getQueryString(name) {
 
 
 
+function  sinaAjax( code, callback ){
+    $.ajax({
+        url:"http://hq.sinajs.cn/list=" + code,
+        dataType: "script",
+        cache: "false",
+        type: "GET",
+        success: function(){
+            var res = {};
+            res.state = true;
+            res.data = new Array();
+            var codeArray = code.split(",");
+            for(var key in codeArray){
+                var codeInfo = eval( "hq_str_" + codeArray[key] );
+                 codeInfo = codeInfo.split(",");
+                var dataObj = {};
+                dataObj.code = codeArray[key].slice(2);
+                dataObj.prod_name = codeInfo[0];
+                dataObj.last_px = (parseFloat( codeInfo[3] )).toFixed(2);
+                dataObj.open_px = (parseFloat( codeInfo[1] )).toFixed(2);
+                dataObj.preclose_px = (parseFloat( codeInfo[2] )).toFixed(2);
+                dataObj.high_px = (parseFloat( codeInfo[4] )).toFixed(2);
+                dataObj.low_px = (parseFloat( codeInfo[5] )).toFixed(2);
+                dataObj.px_change = (codeInfo[3] - codeInfo[2] ).toFixed(2);
+                dataObj.px_change_rate = ( (codeInfo[3] - codeInfo[2] ) / codeInfo[2] * 100 ).toFixed(2);
+                dataObj.buy_px = (parseFloat( codeInfo[6] )).toFixed(2);
+                dataObj.sell_px = (parseFloat( codeInfo[7] )).toFixed(2);
+                dataObj.business_amount = (parseFloat( codeInfo[8] )).toFixed(2);
+                dataObj.business_balance = (parseFloat( codeInfo[9] )).toFixed(2);
+                dataObj.amplitude = (parseFloat( codeInfo[3] )).toFixed(2);
+                res.data.push( dataObj );
+                
+                callback( res );
+
+            }
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
