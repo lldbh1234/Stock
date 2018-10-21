fadf7c79f17dd5f13676c40d4df087495b5f5c35
liangjian <15934854815@163.com>
2018-10-21 23:57:18 +0800

    user withdraw

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -35,10 +35,10 @@ class Test extends Controller
 
     public function test($order_id = null)
     {
-        /*header("Content-type:text/html;charset=utf-8");
+        header("Content-type:text/html;charset=utf-8");
         $withdrawData = [
             "tradeNo" => createStrategySn(),
-            "amount" => 8,
+            "amount" => 100,
             "createAt" => time(),
             "name" => "梁健",
             "card" => "6217004220033901731",
@@ -46,7 +46,7 @@ class Test extends Controller
         dump($withdrawData);
         $response = (new paymentLLpay())->payment($withdrawData);
         dump($response);
-        exit;*/
+        exit;
         /*
         $nickname = cf('nickname_prefix', config("nickname_prefix"));
         $_logic = new \app\admin\logic\UserLogic();
