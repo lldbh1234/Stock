3719e72909053a5badd2380d53ea6da10be870d5
liangjian <15934854815@163.com>
2019-02-01 20:55:27 +0800

    payment

--- application/extra/llpay.php
+++ application/extra/llpay.php
@@ -42,4 +42,18 @@ return [
         'busi_partner'  => '101001',
         'bg_color'      => 'FC5155',
     ],
+
+    'llpay_wap_hcl_config' =>[
+        'oid_partner'   => '201808100002112670',
+        'RSA_PRIVATE_KEY' => EXTEND_PATH . "llpay/wap/key/rsa_hcl_private_key.pem",
+        'key'           => '', //MD5key, 现在已经不支持MD5了
+        'version'       => '1.2',
+        'app_request'   => '3',
+        'sign_type'     => strtoupper('RSA'),
+        'valid_order'   => '10080',
+        'input_charset' => strtolower('utf-8'),//需要放在根目录
+        'transport'     => 'http',
+        'busi_partner'  => '101001',
+        'bg_color'      => 'FC5155',
+    ],
 ];
